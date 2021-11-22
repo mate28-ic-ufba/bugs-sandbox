f5eccf79706fd80bed2cf620aac468f4fe01523d
Levy Santiago <levyssantiago@gmail.com>
2021-11-22 19:54:38 -0300

    adicionando codigo javascript com bugs

---
+++ javascript/Calculator.js
@@ -0,0 +1,22 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function sub(a, b) {
+  return a - b;
+}
+
+function mul(a, b) {
+  return a * b;
+}
+
+function div(a, b) {
+  return a / b;
+}
+
+console.log(sum(10, 2));
+console.log(sub(0, 2));
+console.log(sub(2, 10));
+console.log(div(10, 2));
+console.log(div(10, 0));
+console.log(mul(10, 2));
